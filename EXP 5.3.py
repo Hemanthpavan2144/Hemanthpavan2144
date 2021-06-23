e60aee34188a58ed104803a9220e40de34626936
Hemanthpavan2144 <84568987+Hemanthpavan2144@users.noreply.github.com>
2021-06-22 22:21:59 -0600

    Create EXP 5.3.py

---
+++ EXP 5.3.py
@@ -0,0 +1,22 @@
+'''
+5.3
+Implement a python script to count number of words in a string and reverse each word in a string at the same location. Example:
+'''
+
+input_str = input('Enter a string: ')
+input_str_list = input_str.split(' ')
+count = len(input_str_list)
+
+output_str_list = []
+
+for word in input_str_list:
+	output_str_list.append(word[-1::-1])
+
+print('Word Count:',count)
+print(' '.join(output_str_list))
+
+
+OUTPUT:
+Enter a string: I AM Hemanth
+Word Count: 3
+I MA htnameH
